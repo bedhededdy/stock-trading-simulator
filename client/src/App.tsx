36731d1f51d9e5f7703ab85fa5c053b04cc23de1
Edward Pinkston <edward.pinkston@gmail.com>
2024-04-07 23:59:45 -0500

    Set up routes w/ UserContext

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,34 @@
+import { useEffect, useState } from "react";
+import { BrowserRouter as Router, Routes, Route, useNavigate } from "react-router-dom";
+
+import UserContext from "@contexts/UserContext";
+
+import UserView from "@views/UserView";
+
+import Login from "@components/Login";
+import Dashboard from "@components/Dashboard";
+import Navbar from "@components/Navbar";
+
 
 const App: React.FC = () => {
+  const [user, setUser] = useState<UserView>(new UserView());
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // TODO: CHECK FOR AN EXISTING SESSION ON THE SERVER
+  }, []);
+
   return (
-    <p>Hello world!</p>
+    <Router>
+      <UserContext.Provider value={{ user, setUser }}>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+        </Routes>
+      </UserContext.Provider>
+    </Router>
   )
 }
 
